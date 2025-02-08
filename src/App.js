df136fb3d98bd83d20fafec1a94b505f7801c506
Eugene Larkin <31957364+evglark@users.noreply.github.com>
2025-02-08 22:58:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,24 +12,6 @@ function App() {
     tg.close();
   }
 
-  const createOneMoreMnemonic = () => {
-    setMnemonic(bip39.generateMnemonic());
-  }
-
-  const copyMnemonic = async () => {
-    try {
-      if (navigator.clipboard && window.isSecureContext) {
-        await navigator.clipboard.writeText(mnemonic);
-        alert("Текст скопирован в буфер обмена!");
-      } else {
-        alert("Копирование не поддерживается");
-      }
-    } catch (err) {
-      console.error("Ошибка копирования: ", err);
-      alert("Ошибка копирования! Попробуйте вручную.");
-    }
-  }
-
   useEffect(() => {
     tg.ready();
     setMnemonic(bip39.generateMnemonic());
@@ -38,10 +20,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={onClick}>Close</button>
-      <div>
-        {mnemonic} <button onClick={copyMnemonic}>Copy</button>
-      </div>
-      <button onClick={createOneMoreMnemonic}>One More</button>
+      <div>{mnemonic}</div>
     </div>
   );
 }
